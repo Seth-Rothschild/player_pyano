5dffe60f7b439b5ee5a3a7e4b4ed662d1a3ddeec
Seth Rothschild <seth.rothschild@gmail.com>
2022-04-11 23:07:49 -0400

    Add routes for get_songs, play

--- player_pyano/app.py
+++ player_pyano/app.py
@@ -1,17 +1,58 @@
+import os
+import mido
+
 from flask import Flask
 from waitress import serve
 
 app = Flask(__name__)
 
+LOCK = False
+SONGS_LIST = os.listdir("songs")
+
+output_name = mido.get_output_names()[0]
+PORT = mido.open_output(output_name)
+
 
 @app.route("/")
 def welcome():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>Hello, World!</h1>", 200
+
+
+@app.route("/songs")
+def get_songs():
+    songs = os.listdir("songs")
+    return {"songs_list": SONGS_LIST}, 200
+
+
+@app.route("/play")
+def play():
+    global LOCK
+    if LOCK:
+        return "Please wait.", 425
+    else:
+        LOCK = True
+
+    if not len(SONGS_LIST):
+        return "No songs loaded!", 500
+    path = os.path.join("songs", SONGS_LIST[0])
+    midi_file = mido.MidiFile(path)
+    print("Playing {} ({} Seconds)".format(path, midi_file.length))
+
+    for msg in midi_file.play():
+        PORT.send(msg)
+    LOCK = False
 
 
 def run(port=3000):
-    print("Serving App on Port {}".format(port))
-    serve(app, port=port)
+    try:
+
+        print("Serving App on Port {}".format(port))
+        serve(app, port=port)
+    except KeyboardInterrupt:
+        print("n/Interrupted!")
+    finally:
+        PORT.reset()
+        PORT.close()
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -5,25 +5,19 @@ import os
 
 def read(filename):
     filepath = os.path.join(os.path.dirname(__file__), filename)
-    with io.open(filepath, mode='r', encoding='utf-8') as f:
+    with io.open(filepath, mode="r", encoding="utf-8") as f:
         return f.read().splitlines()
 
 
 setup(
-    name='player_pyano',
+    name="player_pyano",
     packages=find_packages(),
-    version='0.0.1',
-    entry_points={
-        'console_scripts': [
-            'player_pyano = player_pyano.__main__:main'
-        ]
-    },
-    author='Seth Rothschild',
-    author_email='seth.j.rothschild@gmail.com',
-    description='A web application to select and send midi',
-    install_requires=read('requirements.txt'),
-    tests_require=[
-        'pytest'
-    ],
-    test_suite='pytest'
+    version="0.0.1",
+    entry_points={"console_scripts": ["player_pyano = player_pyano.__main__:main"]},
+    author="Seth Rothschild",
+    author_email="seth.j.rothschild@gmail.com",
+    description="A web application to select and send midi",
+    install_requires=read("requirements.txt"),
+    tests_require=["pytest"],
+    test_suite="pytest",
 )
